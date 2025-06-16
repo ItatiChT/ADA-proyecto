a91346de845286f922c140e4c3802538313322f6
Itati <itatichiliguaytorr@mi.unc.edu.ar>
2025-06-16 16:14:46 -0300

    actualizacion

--- Ejercicios clase 8/EjercicioDos.js
+++ Ejercicios clase 8/EjercicioDos.js
@@ -1,10 +1,12 @@
-//1. Pida al usuario que ingrese un número. 
-//2. Verifique si el número es positivo, negativo o igual a cero. 
+//1. Pida al usuario que ingrese un número (dato de entrada)
+//2. Verifique si el número es positivo, negativo o igual a cero (dato de salida)
 //3. Muestre un mensaje indicando cuál es el caso. 
 
 const prompt = require('prompt-sync')({ sigint: true });
 let numero = prompt('Ingrese un numero: ');
 numero = parseFloat (numero)
-if (numero > 0) {console.log ('Positivo')}
+//parsefloat va a convertir lo ingresado a numero, sino podria interpretarse como caracteres
+if (numero > 0) {console.log ('El numero es positivo')}
 else if (numero === 0) {console.log ('Cero')}
+// en este punto al usar solo '=' no funcionaba, despues lei que la igualdad que tenia que establecer era del tipo ===
 else if (numero < 0) { console.log ('Negativo') }; 
